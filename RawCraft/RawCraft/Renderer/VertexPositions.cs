162f6e31e68e7e1b82105a33e18c783d2f1c9e8c
Valdiralita <stefanpoelloth@yahoo.de>
2012-10-18 15:13:11 +0300

    Update RawCraft/RawCraft/Renderer/VertexPositions.cs

--- RawCraft/RawCraft/Renderer/VertexPositions.cs
+++ RawCraft/RawCraft/Renderer/VertexPositions.cs
@@ -32,7 +32,6 @@ namespace RawCraft.Renderer
             Block[1].Add(new Vector3(0.5f, -0.5f, -0.5f));
             Block[1].Add(new Vector3(-0.5f, -0.5f, -0.5f));
 
-
             Block[2] = new List<Vector3>();
             Block[2].Add(new Vector3(0.5f, 0.5f, -0.5f));
             Block[2].Add(new Vector3(0.5f, 0.5f, 0.5f));
@@ -45,7 +44,6 @@ namespace RawCraft.Renderer
             Block[3].Add(new Vector3(-0.5f, -0.5f, -0.5f));
             Block[3].Add(new Vector3(-0.5f, -0.5f, 0.5f));
 
-
             Block[4] = new List<Vector3>();
             Block[4].Add(new Vector3(-0.5f, 0.5f, -0.5f));
             Block[4].Add(new Vector3(-0.5f, 0.5f, 0.5f));
@@ -57,10 +55,6 @@ namespace RawCraft.Renderer
             Block[5].Add(new Vector3(0.5f, -0.5f, 0.5f));
             Block[5].Add(new Vector3(-0.5f, -0.5f, 0.5f));
             Block[5].Add(new Vector3(-0.5f, -0.5f, -0.5f));
-
-
-
-
 
 
             Plant = new List<Vector3>();
@@ -87,92 +81,3 @@ namespace RawCraft.Renderer
         }
     }
 }
-
-
-
-
-
-
-//public static void Initialize()
-//{
-//    // 0 = z+
-//    // 1 = z-
-//    // 2 = x+
-//    // 3 = x-
-//    // 4 = y+
-//    // 5 = y-
-//    Block = new List<Vector3>[6];
-
-//    Block[0] = new List<Vector3>();
-
-//    Block[0].Add(new Vector3(0.5f, 0.5f, 0.5f));
-//    Block[0].Add(new Vector3(-0.5f, 0.5f, 0.5f));
-//    Block[0].Add(new Vector3(-0.5f, -0.5f, 0.5f));
-//    Block[0].Add(new Vector3(0.5f, -0.5f, 0.5f));
-
-//    Block[1] = new List<Vector3>();
-//    Block[1].Add(new Vector3(-0.5f, 0.5f, -0.5f));
-//    Block[1].Add(new Vector3(0.5f, 0.5f, -0.5f));
-//    Block[1].Add(new Vector3(0.5f, -0.5f, -0.5f));
-//    Block[1].Add(new Vector3(-0.5f, -0.5f, -0.5f));
-
-
-//    Block[2] = new List<Vector3>();
-//    Block[2].Add(new Vector3(0.5f, 0.5f, -0.5f));
-//    Block[2].Add(new Vector3(0.5f, 0.5f, 0.5f));
-//    Block[2].Add(new Vector3(0.5f, -0.5f, 0.5f));
-//    Block[2].Add(new Vector3(0.5f, -0.5f, -0.5f));
-
-//    Block[3] = new List<Vector3>();
-//    Block[3].Add(new Vector3(-0.5f, 0.5f, 0.5f));
-//    Block[3].Add(new Vector3(-0.5f, 0.5f, -0.5f));
-//    Block[3].Add(new Vector3(-0.5f, -0.5f, -0.5f));
-//    Block[3].Add(new Vector3(-0.5f, -0.5f, 0.5f));
-
-
-//    Block[4] = new List<Vector3>();
-//    Block[4].Add(new Vector3(-0.5f, 0.5f, -0.5f));
-//    Block[4].Add(new Vector3(-0.5f, 0.5f, 0.5f));
-//    Block[4].Add(new Vector3(0.5f, 0.5f, 0.5f));
-//    Block[4].Add(new Vector3(0.5f, 0.5f, -0.5f));
-
-//    Block[5] = new List<Vector3>();
-//    Block[5].Add(new Vector3(0.5f, -0.5f, -0.5f));
-//    Block[5].Add(new Vector3(0.5f, -0.5f, 0.5f));
-//    Block[5].Add(new Vector3(-0.5f, -0.5f, 0.5f));
-//    Block[5].Add(new Vector3(-0.5f, -0.5f, -0.5f));
-
-
-
-
-
-
-
-//    Plant = new List<Vector3>();
-
-//    Plant.Add(new Vector3(-0.5f, 0.5f, -0.5f));
-//    Plant.Add(new Vector3(0.5f, 0.5f, 0.5f));
-//    Plant.Add(new Vector3(0.5f, -0.5f, 0.5f));
-//    Plant.Add(new Vector3(-0.5f, -0.5f, -0.5f));
-
-//    Plant.Add(new Vector3(0.5f, 0.5f, 0.5f));
-//    Plant.Add(new Vector3(-0.5f, 0.5f, -0.5f));
-//    Plant.Add(new Vector3(-0.5f, -0.5f, -0.5f));
-//    Plant.Add(new Vector3(0.5f, -0.5f, 0.5f));
-
-//    Plant.Add(new Vector3(-0.5f, 0.5f, 0.5f));
-//    Plant.Add(new Vector3(0.5f, 0.5f, -0.5f));
-//    Plant.Add(new Vector3(0.5f, -0.5f, -0.5f));
-//    Plant.Add(new Vector3(-0.5f, -0.5f, 0.5f));
-
-//    Plant.Add(new Vector3(0.5f, 0.5f, -0.5f));
-//    Plant.Add(new Vector3(-0.5f, 0.5f, 0.5f));
-//    Plant.Add(new Vector3(-0.5f, -0.5f, 0.5f));
-//    Plant.Add(new Vector3(0.5f, -0.5f, -0.5f));
-
-
-
-
-
-//}
-
