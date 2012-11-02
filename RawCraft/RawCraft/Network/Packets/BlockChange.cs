a00bcf2a9b5a8aab82d3a211c7b0a37c4476d4fb
Valdiralita <stefanpoelloth@yahoo.de>
2012-11-02 12:25:56 +0100

    quick update

--- RawCraft/RawCraft/Network/Packets/BlockChange.cs
+++ RawCraft/RawCraft/Network/Packets/BlockChange.cs
@@ -7,21 +7,28 @@ namespace RawCraft.Network.Packets
 {
     class BlockChange
     {
+        int x, y, z;
+        byte id, meta;
+
         public BlockChange(EnhancedStream s)
         {
-            int x = s.ReadInt();
-            int y = s.ReadByte();
-            int z = s.ReadInt();
-            byte id = (byte)s.ReadShort();
-            byte meta = (byte)s.ReadByte();
+            x = s.ReadInt();
+            y = s.ReadByte();
+            z = s.ReadInt();
+            id = (byte)s.ReadShort();
+            meta = (byte)s.ReadByte();
 
+            UpdateChunk();
+        }
+
+        private void UpdateChunk()
+        {
             Chunk c;
+            int chunkx = (int)(x / 16 - Math.Sign(x) == -1 ? 1 : -1);
+            int chunkz = (int)(z / 16 - Math.Sign(z) == -1 ? 1 : -1);
 
             // the following if check is wrong, sometimes we receive a block change for a chunk we didnt receive yet.
             // TODO: check and improve performance
-
-            int chunkx = (int)(x / 16 - Math.Sign(x) == -1 ? 1 : -1);
-            int chunkz = (int)(z / 16 - Math.Sign(z) == -1 ? 1 : -1);
 
             if (MapChunks.Chunks.TryGetValue(new Vector2(chunkx, chunkz), out c))
             {
@@ -29,7 +36,6 @@ namespace RawCraft.Network.Packets
 
                 offsetX = x < 0 ? 16 + (x % 16 == 0 ? -16 : x % 16) : x % 16;
                 offsetZ = z < 0 ? 16 + (z % 16 == 0 ? -16 : z % 16) : z % 16;
-
 
                 c.ChangeBlock(new Vector3(offsetX, y, offsetZ), id, meta);
             }
